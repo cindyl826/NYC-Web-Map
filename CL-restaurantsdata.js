8c62d9fc99ab8867983a69286f583064d6789720
cindyl826 <92828455+cindyl826@users.noreply.github.com>
2022-01-07 00:41:48 -0500

    Add files via upload

---
+++ CL-restaurantsdata.js
@@ -0,0 +1,165 @@
+var restaurants = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Sushi Noz",
+        "description": "Compact counter for high-end, seasonal edomae sushi, served omakase-only in a wood-lined space.",
+        "foodType": "$$$$-Japanese",
+        "website": "https://www.sushinoz.com/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.95811547278056,
+          40.774129949750375
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Jean-Georges",
+        "description": "Sophisticated New French eatery boasting floor-to-ceiling windows overlooking Central Park.   ",
+        "foodType": "$$$$-French   ",
+        "website": "https://www.jean-georges.com/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.98160105928804,
+          40.76920383735482
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Masa",
+        "description": "Japanese restaurant offering a high-end fixed-price sushi menu in modern, elegant surrounds. ",
+        "foodType": "$$$$-Japanese   ",
+        "website": "http://www.masanyc.com/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.98305653045226,
+          40.76870051503328
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Per Se",
+        "description": "Chef Thomas Keller's New American restaurant offers luxe fixed-price menus, with Central Park views.",
+        "foodType": "$$$$-French   ",
+        "website": "https://www.thomaskeller.com/perseny"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.98292435928806,
+          40.76842756685278
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Marea",
+        "description": "High-end Italian seafood & housemade pastas from Michael White in a chic Central Park South setting.",
+        "foodType": "$$$$-Italian   ",
+        "website": "https://www.marearestaurant.com/new-york"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.981092015109,
+          40.767631548719784
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Daniel",
+        "description": "Daniel Boulud's elegant French flagship where jackets are required & expense accounts come in handy.",
+        "foodType": "$$$$-French   ",
+        "website": "https://www.danielnyc.com/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.96776371446465,
+          40.76774145223524
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Kochi",
+        "description": "About Kochi  Seasonal ingredients shine on a multicourse Korean tasting menu served in a sleek space.",
+        "foodType": "$$$$-Korean     ",
+        "website": "https://kochinyc.com/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.99344774394493,
+          40.762220115919135
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Le Bernardin",
+        "description": "Elite French restaurant offers chef Eric Ripert's refined seafood, expert service & luxurious decor.",
+        "foodType": "$$$$-French   ",
+        "website": "https://www.le-bernardin.com/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.98175905928828,
+          40.76171911209996
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Aquavit",
+        "description": "High-end Scandinavian tasting & fixed-price menus served in a minimalist Nordic setting. ",
+        "foodType": "$$$$-Scandinavian   ",
+        "website": "https://www.aquavit.org/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.97224551510904,
+          40.76110504619779
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Le Jardinier",
+        "description": "A vegetable-focused menu & sustainable proteins are served in a modern greenhouse-inspired space.",
+        "foodType": "$$$$-French   ",
+        "website": "https://lejardinier-nyc.com/"
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.97124864209411,
+          40.75844883425054
+        ]
+      }
+    }
+  ]
+}
